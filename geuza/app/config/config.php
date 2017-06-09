d24726717ec8c7e04c807df87de78e4716d47522
Amina Kombo <komboamina@gmail.com>
2017-06-09 13:03:32 +0300

    Upload directory files
    Files for 1.3 stable version. Makes the application more visible.

---
+++ geuza/app/config/config.php
@@ -0,0 +1,115 @@
+<?php
+/*
+config.php
+
+Defines important app constants for Geuza
+
+
+@const GEUZALINK -> link to main application. Change it to your own.
+*/
+if(!defined('GEUZALINK')){
+    
+    define('GEUZALINK',"http://localhost/your/path/here/");
+    
+}//if
+
+/*
+@const GEUZAVERSION -> version of current Geuza distro.
+If you remove this, edit the text in app/pages/about and in app/pages/footer.php
+*/
+if(!defined('GEUZAVERSION')){
+    
+    define('GEUZAVERSION','1.3 (stable)');
+
+}//if
+
+/*
+@const APPTITLE -> title of application
+If you remove this, edit the <title> tag in index.php
+*/
+if(!defined('APPTITLE')){
+    
+    define('APPTITLE','Geuza');
+
+}//if
+
+/*
+@const FINANCEURL -> URL of exchange rate website
+Required for the application to work.
+*/
+if(!defined('FINANCEURL')){
+    
+    define('FINANCEURL','https://www.google.com/finance/converter');
+    
+}//if
+
+/*
+@const CURRENCYDATAFILE -> source of currency data
+Required. You may edit this, as long as it's a json file.
+For non-json files, edit app/core/currencymodel.php and app/forms/currency_convert.php
+*/
+if(!defined('CURRENCYDATAFILE')){
+    
+    define('CURRENCYDATAFILE',"app".DS."data".DS."currencies.json");
+    
+}//if
+
+/*
+@const DEFAULTCURRENCY -> default currency code of application
+Required. Removing this will throw errors in app/pages/stage.php
+*/
+if(!defined('DEFAULTCURRENCY')){
+    
+    define('DEFAULTCURRENCY','KES');
+
+}//if
+
+/*
+@const DECIMALS -> number of decimal places to display for conversion
+Required. Removing this will throw errors in conversion forms
+*/
+if(!defined('DECIMALS')){
+    
+    define('DECIMALS',3);
+
+}//if
+
+/*
+@const MINIMUMAMOUNT -> the least amount to compare against
+Required. Removing this will throw errors in conversion forms
+*/
+if(!defined('MINIMUMAMOUNT')){
+    
+    define('MINIMUMAMOUNT',0.01);
+
+}//if
+
+/*
+@const STEP -> value to increment in the 'amount' inut field
+Required. Removing this will throw errors in conversion forms
+*/
+if(!defined('STEP')){
+    
+    define('STEP',0.01);
+
+}//if
+
+/*
+@const COPYRIGHTINFO -> may be changed to your information
+If you remove this, edit app/pages/footer.php
+*/
+if(!defined('COPYRIGHTINFO')){
+    
+    define('COPYRIGHTINFO','&copy; '.date("Y").' Amina Kombo');
+
+}//if
+
+/*
+@const DOWNLOADLINK -> link to Geuza download file. Keep it as it, or change it to your own.
+If you remove this, edit app/pages/header.php and app/pages/footer.php
+*/
+if(!defined('DOWNLOADLINK')){
+    
+    define('DOWNLOADLINK',"https://github.com/AminaKombo/Geuza");
+
+}//if
---
+++ geuza/app/core/geuza.php
@@ -0,0 +1,145 @@
+<?php
+/*
+geuza.php
+
+Class Geuza
+
+Initiliazes system constants
+
+Handles currency conversion
+
+Saves conversion data
+
+*/
+namespace Geuza\Core;
+
+class Geuza{
+    
+    var $from="";
+    
+    var $to="";
+    
+    var $amount=1;
+    
+    var $conversionData=array();
+    
+    /*
+    __constructor()
+    @param configFile -> system configuration file path
+    initializes system constants
+    sets conversionData attribute if previously-submitted form data has been saved
+    */
+    public function __construct($configFile){
+        
+        require($configFile);
+        
+        if(isset($_SESSION['jsend'])){
+            
+            $this->conversionData=$this->setConversionData();
+        
+        }//if
+        
+    }//constructor
+    
+    public function __destruct(){}//destructor
+    
+    
+    /*
+    convertCurrencies() 
+    receives $_POSTed data, converts currencies
+    */
+    public function convertCurrencies(){
+        
+        //save submitted form data
+        $_SESSION['grecover']['from']=$_POST['from'];
+        
+        $_SESSION['grecover']['to']=$_POST['to'];
+        
+        $_SESSION['grecover']['amount']=$_POST['amount'];
+        
+        $this->from=urlencode($_POST['from']);
+        
+        $this->to=urlencode($_POST['to']);
+        
+        $this->amount=$_POST['amount'];
+        
+        if($_POST['to']!==$_POST['from']){
+            
+            //call finance script, receive exchange rate data
+            $get=file_get_contents(FINANCEURL."?a=".$this->amount."&from=".$this->from."&to=".$this->to);
+
+            $get=explode("<span class=bld>",$get);
+
+            if(isset($get[1])){//format returned exchange rate data
+
+                $get=explode("</span>",$get[1]);
+
+                $rate = preg_replace("/[^0-9\.]/", null, $get[0]);
+
+                $converted_amount = $this->amount*$rate;
+
+            }//if
+            else{
+            
+                $rate=0;
+
+                $converted_amount=0;
+
+            }//else
+            
+        }//if
+        if($_POST['to']===$_POST['from']){//no conversion between same currency
+            
+            $rate=1;
+            
+            $converted_amount=1;
+            
+        }//if
+        
+        
+        $data = array( 'rate' => $rate, 
+                        'converted_amount' =>$converted_amount, 
+                        'from_Currency' => strtoupper($this->from), 
+                        'to_Currency' => strtoupper($this->to)
+                    );
+
+        $_SESSION['jsend']['from']=$this->from;
+
+        $_SESSION['jsend']['to']=$this->to;
+
+        $_SESSION['jsend']['amount']=$_POST['amount'];
+
+        $_SESSION['jsend']['converted_amount']=$converted_amount;
+
+        $_SESSION['jsend']['rate']=$rate;
+                                 
+        $this->setConversionData=$data;
+        
+    }//convertCurrencies
+    
+    /*
+    setConversionData()
+    @param mixed[] data -> array of currency conversion information
+    @return mixed[] data -> array of (recovered) currency conversion information
+    */
+    private function setConversionData($data=null){
+        
+        if(isset($_SESSION['jsend'])){
+            
+            $data['rate']=$_SESSION['jsend']['rate'];
+            
+            $data['from_Currency']=$_SESSION['jsend']['from'];
+            
+            $data['to_Currency']=$_SESSION['jsend']['to'];
+            
+            $data['converted_amount']=$_SESSION['jsend']['converted_amount'];
+            
+            $this->amount=$_SESSION['jsend']['amount'];
+            
+        }//if
+        
+        return $data;
+        
+    }//setConversionData
+    
+}//geuza
